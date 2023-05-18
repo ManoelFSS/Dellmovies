55ae071922b5d7a28786a1e63024ee7af95ec664
Manoel <piloukos45@gmail.com>
2023-05-18 18:53:36 -0300

    teste btn

--- src/components/header/styled.jsx
+++ src/components/header/styled.jsx
@@ -168,10 +168,10 @@ export const Conteiner_btns = styled.div`
     margin-top: 20px;
     max-height: 100px;
     gap: 10px;
-    
+
     @media(max-width:450px){
         margin-top: 20px;
-        margin-bottom:50px;
+        margin-bottom:70px;
         max-height: 100px;
     }
  
